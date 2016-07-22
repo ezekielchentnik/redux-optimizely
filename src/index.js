53c1274c142edeb3400f2fd66c1a0bdcecb64d12
ezekielchentnik <zeek@ezekielchentnik.com>
2016-07-21 20:23:24 -0500

    update

--- src/index.js
+++ src/index.js
@@ -2,7 +2,11 @@ import reporter from 'redux-reporter';
 
 export default reporter(({ type, payload }) => {
 
-    window.optimizely = window.optimizely || [];
-    window.optimizely.push(['trackEvent', type, payload]);
+    try {
+        window.optimizely = window.optimizely || [];
+        window.optimizely.push(['trackEvent', type, payload]);
+    } catch (err) {
+        console.error(err);
+    }
 
 }, ({ meta = {} }) => meta.experiments);
